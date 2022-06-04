5dba65086e65f682d2b66b758986285423d02e77
iStef <stefan_b_k@icloud.com>
2022-06-04 19:45:13 +0300

    init + refactor with v-for for radio buttons

---
+++ src/components/survey/LearningSurvey.vue
@@ -0,0 +1,82 @@
+<template>
+  <base-card>
+    <h2>How was you learning experience?</h2>
+
+    <form @submit.prevent="submitSurvey">
+
+      <div class="form-control">
+        <label for="name">Your Name</label>
+        <input type="text" id="name" v-model.trim="form.enteredName" />
+      </div>
+
+      <h3>My learning experience was ...</h3>
+      <div v-for="(rating, key) in ratings" :key="key" class="form-control">
+        <input type="radio" :id="key" :value="key" v-model="form.chosenRating" />
+        <label :for="key">{{ rating }}</label>
+      </div>
+
+      <p v-if="form.invalidInput">
+        One or more input fields are invalid. Please check your provided data.
+      </p>
+
+      <div>
+        <base-button>Submit</base-button>
+      </div>
+
+    </form>
+
+  </base-card>
+</template>
+
+
+<script>
+const FIREBASE_DB = 'https://vue-http-demo-d316d-default-rtdb.europe-west1.firebasedatabase.app/';
+export default {
+  data () {
+    return {
+      form: { ...this.formDefaults() },
+      ratings: { poor: 'Poor', average: 'Average', great: 'Great' }
+    };
+  },
+  methods: {
+    submitSurvey () {
+      if (this.form.enteredName === '' || !this.form.chosenRating) {
+        this.form.invalidInput = true;
+        return;
+      }
+      fetch(FIREBASE_DB + 'surveys.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: this.form.enteredName,
+          rating: this.form.chosenRating
+        })
+      });
+
+      this.form = { ...this.formDefaults() };
+    },
+    formDefaults () {
+      return {
+        enteredName: '',
+        chosenRating: null,
+        invalidInput: false
+      };
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+.form-control {
+  margin: 0.5rem 0;
+}
+
+input[type='text'] {
+  display: block;
+  width: 20rem;
+  margin-top: 0.5rem;
+}
+</style>
---
+++ src/components/survey/UserExperiences.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button>Load Submitted Experiences</base-button>
+      </div>
+      <ul>
+        <survey-result
+          v-for='result in results'
+          :key='result.id'
+          :result='result'
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  props: ['results'],
+  components: { SurveyResult }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
